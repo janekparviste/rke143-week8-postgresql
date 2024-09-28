583bbf9943e49a18c741b8cba73075c21adc8fac
JanekParviste <janek.parviste@tktk.ee>
2024-09-28 04:08:46 +0300

    rke143 week8 postgresql examples

---
+++ sqldatabase.sql
@@ -0,0 +1,56 @@
+CREATE TABLE recipe (
+    id SERIAL PRIMARY KEY,
+    recipeName VARCHAR(55)
+);
+
+CREATE TABLE ingredient (
+    id SERIAL PRIMARY KEY,
+    ingredientName VARCHAR(55)
+);
+
+CREATE TABLE IngredientInRecipe (
+    id SERIAL PRIMARY KEY,
+    recipeId INTEGER,
+    ingredientId INTEGER,
+    FOREIGN KEY (recipeID) REFERENCES recipe(id),
+    FOREIGN KEY (ingredientId) REFERENCES ingredient(id)
+);
+
+INSERT INTO recipe (recipeName) 
+VALUES ('Pumpkin Pasties');
+
+SELECT * FROM recipe;
+
+DELETE FROM recipe where id=2;
+
+INSERT INTO recipe (recipeName) 
+VALUES ('Pumpkin Tartlets');
+
+INSERT INTO recipe (recipeName) 
+VALUES ('Creamy Pumpkin Soup');
+
+
+INSERT INTO ingredient (ingredientName) 
+VALUES ('pumpkin puree');
+
+INSERT INTO ingredient (ingredientName) 
+VALUES ('sugar');
+
+SELECT * FROM ingredient;
+
+DROP TABLE recipe;
+
+INSERT INTO ingredient (ingredientName) 
+VALUES 
+('cinnamon'),
+('nutmeg'),
+('cloves'),
+('Pastry dough'),
+('Egg wash (1 egg beaten with a splash of milk)'),
+('brown sugar'),
+('ginger'),
+('Mini tart shells'),
+('Whipped cream for garnish'),
+('onion, chopped'),
+('garlic, minced'),
+('vegetable broth');
